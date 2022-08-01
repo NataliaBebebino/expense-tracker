2d315a3792bfabf58cd6cd47f533e5a18798c3ee
NataliaBebebino <nbebebino@gmail.com>
2022-07-31 20:44:57 -0400

    Create Card component which acts as a wrapper

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+
+//this Card component will act as a shell for both our expense items or our expenses content, it will be like a wrapper component.
+function Card(props) {
+    const classes = "card " + props.className
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
